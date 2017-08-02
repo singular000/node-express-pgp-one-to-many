593da2ed0b3de3998ad340a3d53b3b24594312b7
Thom Page <thom.page.000@gmail.com>
2017-08-02 16:26:26 -0400

    adds async/await and conventional handling to notes controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -8,44 +8,41 @@ const db = require('../db/connect_db');
 const Note = require('../models/index').notes;
 
 // routes
-router.get('/', (req, res) => {
-  db.any(Note.all)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'found all notes' })
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failed', err: err.message, message: 'could not get all notes' })
-    }); 
+router.get('/', async (req, res) => {
+  try {
+    const data = await db.any(Note.all);
+    res.status(200).json({ success: true, data, message: 'retrieved all notes' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not get all notes' });
+  }
 });
 
-router.post('/', (req, res) => {
-  db.one(Note.add, req.body)
-    .then(data => {
-      res.status(201).json({ status: 'success', data: data, message: 'created a note!' })
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failed', err: err.message, message: 'could not create note' })
-    });
+router.post('/', async (req, res) => {
+  try {
+    const data = await db.one(Note.add, req.body);
+    res.status(201).json({ success: true, data, message: 'created a note' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not create note' });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   req.body.id = req.params.id;
-  db.one(Note.update, req.body)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'updated a note' });
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failure', err: err.message, message: 'failed to update note' });
-    })
+  try {
+    const data = await db.one(Note.update, req.body);
+    res.status(200).json({ success: true, data, message: 'updated a note' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not update note' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  db.one(Note.remove, req.params.id)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'deleted a note' });
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failure', err: err.message, message: 'could not delete note' });
-    })
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = db.one(Note.remove, req.params.id);
+    res.status(200).json({ success: true, data, message: 'deleted a note' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not delete note' });
+  }
 });
+
 module.exports = router;
