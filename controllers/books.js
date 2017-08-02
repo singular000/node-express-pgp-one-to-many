d392d0b4474e4133def161bbb3f536dc42688a28
Thom Page <thom.page.000@gmail.com>
2017-08-02 16:25:50 -0400

    adds async/await to books controller

--- controllers/books.js
+++ controllers/books.js
@@ -9,56 +9,52 @@ const Book = require('../models/index').books;
 const Note = require('../models/index').notes;
 
 // routes
-router.get('/', (req, res) => {
-  db.any(Book.all)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'found all books' })
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failed', err: err.message, message: 'could not get all books' })
-    }); 
-});
-
-// chained queries
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const book = await db.one(Book.find, req.params.id);
-    book.notes = await db.any(Note.find, req.params.id);
-    res.status(201).json({ status: 'success', data: book, message: 'found a book' })
-  } catch(err) {
-    res.status(400).json({ status: 'failure', data: err.message, message: 'could not get book' })
+    const data = await db.any(Book.all);
+    res.status(200).json({ success: true, data, message: 'retrieved all books' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not get all books' });
   }
 });
 
-router.post('/', (req, res) => {
-  db.one(Book.add, req.body)
-    .then(data => {
-      res.status(201).json({ status: 'success', data: data, message: 'created book' })
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failed', err: err.message, message: 'could not create book' })
-    }); 
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await db.one(Book.find, req.params.id);
+    book.notes = await db.any(Note.search, req.params.id);
+    res.status(200).json({ success: true, data: book, message: 'found a book' });
+  } catch(err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not get book' });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const data = await db.one(Book.add, req.body);
+    res.status(201).json({ success: true, data, message: 'created a book' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not create book' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
   req.body.id = req.params.id;
-  db.one(Book.update, req.body)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'updated book' })
-    })
-    .catch(err => {
-      res.status(422).json({ status: 'failed', err: err.message, message: 'could not update book' })
-    }); 
+  try {
+    const data = await db.one(Book.update, req.body);
+    res.status(200).json({ success: true, data, message: 'updated a book' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not update book' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  db.one(Book.remove, req.params.id)
-    .then(data => {
-      res.status(200).json({ status: 'success', data: data, message: 'deleted book' })
-    })
-    .catch(err => {
-      res.status(400).json({ status: 'failed', err: err.message, message: 'could not delete book' })
-    }); 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = db.one(Book.remove, req.params.id);
+    res.status(200).json({ success: true, data, message: 'deleted a book' });
+  } catch (err) {
+    res.status(400).json({ success: false, err: err.message, message: 'could not delete book' });
+  }
 });
 
 module.exports = router;
+
